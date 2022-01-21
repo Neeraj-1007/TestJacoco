f7c9f65b305b1528df06f6fc477dd4fd071c32e7
Neeraj-1007 <neerajkumarjha10@gmail.com>
2022-01-21 18:21:53 +0530

    command for creating jacoco.xml file

--- spring-sonarCube/src/main/java/com/test/springsonarCube/SpringSonarCubeApplication.java
+++ spring-sonarCube/src/main/java/com/test/springsonarCube/SpringSonarCubeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringSonarCubeApplication {
 
 	public static void main(String[] args) {
+		System.out.println("use command clean org:jacoco:jacoco-maven-plugin:prepare-agent install");
 		SpringApplication.run(SpringSonarCubeApplication.class, args);
 	}
 
